bcb7b7977f88e1d45fc1cdfd3ecccc513258012e
Heavensliving <heavensliving@gmail.com>
2024-10-04 20:29:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 // App.jsx
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import MainAPortal from './pages/MainAPortal';
 
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/florainndotheavens-adminpage" element={<MainAPortal />} />
     </Routes>
   );
 }
